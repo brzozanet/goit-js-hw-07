9cd39beefff872bd4f3e607c9a61d43549983676
Paweł Brzozowski <pawel@brzoza.net>
2023-04-04 12:16:11 +0200

    Task #02 (SimpleLightbox)

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+let imageFromLightbox;
+
+for (const item of galleryItems) {
+  const imageEl = document.createElement("li");
+  imageEl.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <a class="gallery__item" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+    </a>
+    `
+  );
+  galleryEl.append(imageEl);
+};
+
+const showImage = event => {
+  if (event.target.nodeName !== "IMG") {
+    return;
+  } else {
+    event.preventDefault();
+    imageFromLightbox = new SimpleLightbox(".gallery a", {
+      captionsData: "alt",
+      captionDelay: 250,
+    });
+
+  }
+};
+
+galleryEl.addEventListener("click", showImage);
